06a46c9f85b90e2c2142fa49f41f43db961d682f
Claudio <claudiosfcarvalho@outlook.com>
2020-07-13 23:40:05 -0300

    read

--- src/main/java/com/claudiowork/readwritefile/controller/FileController.java
+++ src/main/java/com/claudiowork/readwritefile/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.claudiowork.readwritefile.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +21,18 @@ public class FileController {
 	@GetMapping("/{nomeArquivo}")
 	public String readFile(@PathVariable("nomeArquivo") String fileIn) {
 		System.out.println(fileIn);
-		return fs.readFile(fileIn);
+		return "teste";
 	}
 
-	@GetMapping("read")
-	public String readFile(@RequestBody FileConfig fileConfig) {
+	@GetMapping("/readAllOneLine")
+	public String readFileAllOneLine(@RequestBody FileConfig fileConfig) {
 		String fullName = fileConfig.getDiretorioArquivo() + fileConfig.getNomeArquivo();
-		return fs.readFile(fullName);
+		return fs.readFileAllOneLine(fullName);
+	}
+	
+	@GetMapping("/readAll")
+	public List<String> readFileAll(@RequestBody FileConfig fileConfig) {
+		String fullName = fileConfig.getDiretorioArquivo() + fileConfig.getNomeArquivo();
+		return fs.readFileAll(fullName);
 	}
 }
--- src/main/java/com/claudiowork/readwritefile/service/FileService.java
+++ src/main/java/com/claudiowork/readwritefile/service/FileService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -11,27 +13,19 @@ import org.springframework.stereotype.Service;
 public class FileService {
 
 	private FileReader fr;
+	private BufferedReader objReader;
 
 	public void setFileReader(String nomeArq) throws FileNotFoundException {
 		fr = new FileReader(nomeArq);
 	}
 
-	public String readFile(String file) {
-		if (!file.isEmpty() && fr == null) {
-			try {
-				setFileReader(file);
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-				return "Erro ao carregar o arquivo - " + e1.getMessage();
-			}
-		}
-		BufferedReader objReader = null;
+	public String readFileAllOneLine(String file) {
+
 		StringBuilder sb = new StringBuilder();
 		try {
 			String strCurrentLine;
 
-			objReader = new BufferedReader(fr);
+			objReader = readFile(file);
 
 			while ((strCurrentLine = objReader.readLine()) != null) {
 				sb.append(strCurrentLine);
@@ -52,7 +46,42 @@ public class FileService {
 				ex.printStackTrace();
 			}
 		}
-		
+
 		return sb.toString();
+	}
+
+	private BufferedReader readFile(String file) throws FileNotFoundException {
+		if (!file.isEmpty() && fr == null) {
+			setFileReader(file);
+		}
+		objReader = new BufferedReader(fr);
+		return objReader;
+	}
+
+	public List<String> readFileAll(String file) {
+		List<String> strList = new ArrayList<>();
+		String strReaded = new String("");
+		try {
+			readFile(file);
+			while ((strReaded = objReader.readLine()) != null) {
+				strList.add(strReaded);
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			strList.add("Erro ao carregar o arquivo - " + e1.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			strList.add("Erro ao ler o arquivo - " + e.getMessage());
+		} finally {
+
+			try {
+				if (objReader != null)
+					objReader.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return strList;
 	}
 }
